880d0b822ceb5e468a266bc31fc548555c4aa862
vins790 <insene790@gmail.com>
2017-03-17 10:01:12 +0100

    0.1.2
    Connecting and fetching data from test DB (userList)

--- Bike/users.php
+++ Bike/users.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Maciek";
+include 'connect.php';
+
+        if( $data = $mysqli->query("SELECT login FROM konto") ) {
+            while ($row = $data->fetch_assoc()) { 
+                echo "<tr><td>";
+                echo $row['login'];
+                echo    "</td></tr>";
+            }
+            $data->close();
+        }
+         
 ?>
