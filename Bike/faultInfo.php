3a24b3e92f3c70f1ce3179dfa5a7b3739d2b8914
vins790 <insene790@gmail.com>
2017-03-17 16:59:02 +0100

    0.1.4
    Dorking on detecting clicked table row.

---
+++ Bike/faultInfo.php
@@ -0,0 +1,12 @@
+<?php
+include 'connect.php';
+        $id
+        if( $data = $mysqli->query("SELECT , status FROM awarie") ) {
+            
+            while ($row = $data->fetch_assoc()) { 
+              
+            }
+            $data->close();
+        }
+         
+?>
