1b86c74cb5064a04a199c01fbe888d4f13283a2f
bagin13 <baginski.szymon@gmail.com>
2017-04-04 18:22:31 +0200

    layout logowania

--- App/app/src/main/java/com/example/torak28/app/MainActivity.java
+++ App/app/src/main/java/com/example/torak28/app/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.torak28.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mUsername, mPassword;
+    private Button mOkBtn, mCancleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsername = (EditText)findViewById(R.id.text_user);
+        mPassword = (EditText)findViewById(R.id.text_password);
+    }
+
+    public void login(View view) {
+        //TODO logowanie
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void create_account(View view) {
+        //TODO tworzenie konta
+    }
+
+    public void recoll_password(View view) {
+        //TODO przypominanie hasła
     }
 }
