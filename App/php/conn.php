f1ee705ccded05b69232f1f5260b70795a955ee9
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-05-10 22:14:51 +0200

    Porzadkowanie

--- App/php/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_name = "rentabik_db";
-$mysql_username = "rentabik_db";
-$mysql_password = "trudnehaslo";
-$server_name = "s23.zenbox.pl";
-$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-$conn->set_charset("utf8");
-?>
